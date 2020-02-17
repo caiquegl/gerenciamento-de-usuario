f268178c0159faa8b394bbe21e34130dd7246d4d
caiquegl <arq.caique@hotmail.com>
2020-02-17 20:52:15 -0300

    seguranã js e html feita

--- controlers/UserControler.js
+++ controlers/UserControler.js
@@ -21,6 +21,10 @@ class UserControler {
 
             let values = this.getValues();
 
+            if (!values){
+                return false
+            }
+
             this.getPhoto().then(
                 (content) => {
 
@@ -89,8 +93,16 @@ class UserControler {
     getValues() {
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if (["name", "email", "password"].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add("has-error");
+                isValid = false;
+
+            }
 
             if (field.name == "gender") {
 
@@ -111,6 +123,12 @@ class UserControler {
             }
 
         });
+
+        if (!isValid){
+
+            return false
+
+        }
 
         return new User(
             user.name,
