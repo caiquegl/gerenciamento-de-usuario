efd82adb92d97f80e86b6bf48bd75612a69bf8e7
caiquegl <arq.caique@hotmail.com>
2020-02-17 18:14:13 -0300

    config de photo efetuado, iniciando as proximas

--- controlers/UserControler.js
+++ controlers/UserControler.js
@@ -15,19 +15,55 @@ class UserControler{
 
             event.preventDefault();
 
-            this.addLine(this.getValues())
-            
+            let values = this.getValues();
+
+            this.getPhoto((content)=>{
+
+                values.photo = content;
+
+                this.addLine(values);
+
+            });           
         
         });
         
 
     }
 
+    getPhoto(callback){
+
+        let fileReader = new FileReader();
+
+        let elements = [...this.formEl.elements].filter(item=>{
+
+            if (item.name == "photo") {
+                
+                return item
+            
+            }
+
+        })
+
+        let file = elements[0].files[0];
+
+        fileReader.onload = ()=>{
+
+            callback(fileReader.result);
+
+        };
+
+        fileReader.readAsDataURL(file);
+
+        
+
+    }
+
+
     getValues(){
 
         let user = {};
 
-        this.formEl.elements.forEach(function(field,index){
+        [...this.formEl.elements].forEach(function(field,index){
 
             if (field.name == "gender") {
         
@@ -62,7 +98,7 @@ class UserControler{
 
     this.tableEl.innerHTML = `
                     <tr>
-                        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                        <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
                         <td>${dataUser.email}</td>
                         <td>${dataUser.admin}</td>
